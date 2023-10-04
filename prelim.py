b93aa7f1a321761e2bd02b579aaa53d68be4cc37
qlamv <qlamvirgo@tip.edu.ph>
2023-10-04 09:59:07 +0000

    commit

---
+++ prelim.py
@@ -0,0 +1,11 @@
+def test_multiply():
+
+    cel = 50
+    far = (cel*1.8) + 32
+    assert far == 122
+
+def test_multiply2():
+
+    cel = 45
+    far = (cel*1.8) + 32
+    assert far == 122
---
+++ prelimexam.py
@@ -0,0 +1,11 @@
+import math
+
+def test_square():
+
+    side = 6
+    assert side*side == 36
+
+def test_square2():
+
+    side = 5
+    assert side*side == 36
